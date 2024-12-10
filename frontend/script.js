a820d1243f624052994569024ab2eeb427342db0
PromiseUantioje <promiseuantioje@gmail.com>
2024-12-10 01:21:06 +0100

    Making more progress

--- frontend/script.js
+++ frontend/script.js
@@ -21,4 +21,10 @@ document.getElementById('registrationForm').addEventListener('submit', async fun
      })
 
 
+        const menuIcon = document.querySelector('.menu-icon');
+        const navbar = document.querySelector('.navbar');
+                
+        menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
        
