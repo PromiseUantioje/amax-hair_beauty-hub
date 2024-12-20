624c88a561e05c1988b56ee073a307c4b2aff801
Ayomide Oladipupo <oladipupoa50@gmail.com>
2024-12-20 14:15:24 +0100

    cors defined

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 const path = require('path');
 
 // Import Routes
